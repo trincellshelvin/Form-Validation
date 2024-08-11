e14f8029c9512f72ef4c857965ded01132951550
trincellshelvin <trincellshelvin@gmail.com>
2024-08-11 12:45:55 +0000

    completed html

---
+++ script.js
@@ -0,0 +1,32 @@
+function tooShort(userName){
+    if(userName.length < 5){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function hasI(userName){
+    if(userName.includes("i")){
+        return true;
+    } else{
+        return false
+    }
+}
+
+let tooShort = document.getElementById("tooShort");
+let hasI = document.getElementById("hasI");
+let firstNameInput = document.getElementById("firstName");
+
+function handleChangeinuserName(){
+    console.log("Hello!")
+    let input = firstNameInput.value;
+    let isShort = tooShort(input);
+    if(isShort){
+        tooShort.style.color = "green";
+        console.log("&#x2713 Username is meets requirements!")
+    } else{
+        tooShort.style.color = "pink";
+        console.log("Username must be more than 5 characters")
+    }
+}
