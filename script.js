ed3e373e62517d2fb9507f68d06f46f9fc65fd19
trincellshelvin <trincellshelvin@gmail.com>
2024-08-11 13:24:17 +0000

    completed functions for email field

--- script.js
+++ script.js
@@ -43,3 +43,8 @@ function handleChangeInI(){
 
 firstNameInput.addEventListener("input", handleChangeinuserName)
 firstNameInput.addEventListener("input", handleChangeInI)
+
+function validateEmailInput(email) {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
+}
